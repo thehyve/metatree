53d8e5cee59d65260a3d004e1d8d6b75dffbc01e
Frank <29271979+FrankyHollywood@users.noreply.github.com>
2022-11-10 14:10:12 +0100

    Rename SILS-Fairspace to Metatree (#105)
    * renamed repository
    
    * renamed fairspace_url
    
    * rename of FAIRSPACE_URL
    
    * renamed deployment artifacts
    
    * logo
    
    * image
    
    * metatree image
    
    * disabled logo
    
    * changed some fairspace -> metatree naming, after PR comment
    
    * changed some fairspace -> metatree naming, after PR comment

--- docker/keycloak/setup-realm.sh
+++ docker/keycloak/setup-realm.sh
@@ -1,5 +1,5 @@
 cat /tmp/realm-template.json | \
   sed "s|\${KEYCLOAK_REALM}|${KEYCLOAK_REALM}|" | \
   sed "s|\${KEYCLOAK_CLIENT_ID}|${KEYCLOAK_CLIENT_ID}|" | \
-  sed "s|\${FAIRSPACE_URL}|${FAIRSPACE_URL}|" \
+  sed "s|\${METATREE_URL}|${METATREE_URL}|" \
   > /tmp/realm-export.json
